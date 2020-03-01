cc0b9e3e487939a538e3848a8a46ef34b04e24ee
Lmpwybb <cartoux.sebastien@orange.fr>
2020-03-01 16:14:45 +0000

    Correction d'indentation

--- csv/csv.php
+++ csv/csv.php
@@ -1,49 +1,49 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8"> 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>CSV</title>
-	<link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSV</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<div id="form">
-		<form action="upload.php" method="post" enctype="multipart/form-data">
-			<div class="select">
-				<label for="file">Sélectionner le fichier .CSV à envoyer:</label>
-				<input type="file" id="file" name="file" accept=".csv">
-			</div>
-			<div class="pos">
-				<input type="submit" name="upload" value="Importer le fichier">
-			</div>
-			<div class="pos error">
-				<?php
-					if (isset($_GET['error'])) {
-						if ($_GET['error'] == 'exist') {
-							echo "Le fichier existe déjà.";
-						}
-						if ($_GET['error'] == 'filerror') {
-							echo "Uniquement les fichiers .csv sont autorisés.";
-						}
-						if ($_GET['error'] == 'import') {
-							echo "Il y a eu une erreur lors de l'importation.";
-						}
-						if ($_GET['error'] == 'content') {
-							echo "Le contenu de ce fichier n'est pas de type csv.";
-						}
-					}
-				?>
-			</div>
-			<div class="pos success">
-            <?php
-                if (isset($_GET['success']) && $_GET['success'] == 'import') {
-                    echo "Le fichier a bien été importé.<br>";
-                    echo "La table a bien été crée dans la base de donnée.<br>";
-                    echo "EN COURS.";
-                }
-            ?>
-			</div>
-		</form>
-	</div>
+    <div id="form">
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <div class="select">
+                <label for="file">Sélectionner le fichier .CSV à envoyer:</label>
+                <input type="file" id="file" name="file" accept=".csv">
+            </div>
+            <div class="pos">
+                <input type="submit" name="upload" value="Importer le fichier">
+            </div>
+            <div class="pos error">
+                <?php
+                    if (isset($_GET['error'])) {
+                        if ($_GET['error'] == 'exist') {
+                            echo "Le fichier existe déjà.";
+                        }
+                        if ($_GET['error'] == 'filerror') {
+                            echo "Uniquement les fichiers .csv sont autorisés.";
+                        }
+                        if ($_GET['error'] == 'import') {
+                            echo "Il y a eu une erreur lors de l'importation.";
+                        }
+                        if ($_GET['error'] == 'content') {
+                            echo "Le contenu de ce fichier n'est pas de type csv.";
+                        }
+                    }
+                ?>
+            </div>
+            <div class="pos success">
+                <?php
+                    if (isset($_GET['success']) && $_GET['success'] == 'import') {
+                        echo "Le fichier a bien été importé.<br>";
+                        echo "La table a bien été crée dans la base de donnée.<br>";
+                        echo "EN COURS.";
+                    }
+                ?>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
